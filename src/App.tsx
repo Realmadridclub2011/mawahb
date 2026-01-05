47413aff4b7c304bef2ceaf91b5ca16e2f9d0400
Realmadridclub2011 <eng.mohamdkamal1987@gmail.com>
2026-01-05 14:22:53 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -529,15 +529,30 @@ function MainApp() {
     }}
   />
 )}
-}
-          {currentSection === 'talents' && (
-            <>
-              {page === 'talents' && <TalentsHomePage setPage={setPage} setSelectedCategory={setSelectedCategory} />}
-              {page === 'subcategories' && <SubcategoriesPage categoryId={selectedCategory!} setPage={setPage} setSelectedSubcategory={setSelectedSubcategory} setSelectedCategory={setSelectedCategory} />}
-              {page === 'register' && <RegisterPage categoryId={selectedCategory!} subcategoryId={selectedSubcategory!} setPage={setPage} />}
-              {page === 'mypage' && <MyPage setPage={setPage} />}
-            </>
-          }
+
+{currentSection === "talents" && (
+  <>
+    {page === "talents" && (
+      <TalentsHomePage setPage={setPage} setSelectedCategory={setSelectedCategory} />
+    )}
+    {page === "subcategories" && (
+      <SubcategoriesPage
+        categoryId={selectedCategory!}
+        setPage={setPage}
+        setSelectedSubcategory={setSelectedSubcategory}
+        setSelectedCategory={setSelectedCategory}
+      />
+    )}
+    {page === "register" && (
+      <RegisterPage
+        categoryId={selectedCategory!}
+        subcategoryId={selectedSubcategory!}
+        setPage={setPage}
+      />
+    )}
+    {page === "mypage" && <MyPage setPage={setPage} />}
+  </>
+)}
           {currentSection === 'announcements' && <AnnouncementsPage />}
           {currentSection === 'honors' && <HonorsPage />}
         </div>
