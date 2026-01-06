3c8a7cf04060f13e49ce00e8b701d356afaf6fc9
Realmadridclub2011 <eng.mohamdkamal1987@gmail.com>
2026-01-06 08:40:58 +0300

    Updated AnnouncementsPage.tsx

--- src/pages/AnnouncementsPage.tsx
+++ src/pages/AnnouncementsPage.tsx
@@ -259,7 +259,7 @@ export default function AnnouncementsPage({
                       className={`
                         w-full py-3 rounded-xl text-center
                         text-sm font-black
-                        bg-emerald-50
+                        bg-slate-100
                         border border-white/60
                         ring-1 ring-emerald-300/60
                         ${isComp(a) && !isOpen(a) ? "text-slate-800" : "text-slate-700"}
