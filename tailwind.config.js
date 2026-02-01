0712bf8455385a50d85652b392a470a3c27cb12e
Realmadridclub2011 <eng.mohamdkamal1987@gmail.com>
2026-02-01 11:28:53 +0400

    Updated index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,7 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          "Tajawal", // أو "IBM Plex Sans Arabic"
-          "ui-sans-serif",
-          "system-ui",
-          "-apple-system",
-          "Segoe UI",
-          "Roboto",
-          "Arial",
-          "Noto Sans",
-          "Apple Color Emoji",
-          "Segoe UI Emoji",
-        ],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
